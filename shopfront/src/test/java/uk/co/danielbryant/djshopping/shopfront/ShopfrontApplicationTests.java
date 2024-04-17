cdf423c9015a17b2c395b489c131f24850987ba1
sreekanth-rajan <sreekanthrajan321@gmail.com>
2024-04-17 12:39:42 +0530

    deleted and updated

--- shopfront/src/test/java/uk/co/danielbryant/djshopping/shopfront/ShopfrontApplicationTests.java
+++ shopfront/src/test/java/uk/co/danielbryant/djshopping/shopfront/ShopfrontApplicationTests.java
@@ -5,12 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ShopfrontApplication.class)
-public class ShopfrontApplicationTests {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ShopfrontApplication.class)
+// public class ShopfrontApplicationTests {
 
-    // @Test
-    // public void contextLoads() {
-    // }
+//     // @Test
+//     // public void contextLoads() {
+//     // }
 
-}
+// }
